8d24f8e622714eac7674d674cd80cd68e5751a77
LuisCarrilloP <jl.carrillopadron@gmail.com>
2022-06-30 11:27:40 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,107 @@
+                    /* __________ MENU SHOW/HIDDEN _____________ */
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close")             
+
+      /* MENU SHOW */
+      /* validate if constants exists */
+if(navToggle){
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu")
+    })
+}
+
+    /* MENU HIDDEN */
+    /* validate if constant exists */
+if(navClose){
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu")
+    })
+}
+
+            /*________ REMOVE MENU MOBILE _______ */
+const navLink = document.querySelectorAll(".nav__link")
+
+function linkAction(){
+    const navMenu = document.getElementById("nav-menu")
+    //click and remove
+    navMenu.classList.remove("show-menu")
+}
+navLink.forEach(n => n.addEventListener("click", linkAction))
+
+                /*____ SKILLS ______*/
+const skillsContent = document.getElementsByClassName("skills__content"),
+      skillsHeader = document.querySelectorAll(".skills__header")
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = "skills__content skills__close"
+    }
+    if(itemClass === "skills__content skills__close"){
+        this.parentNode.className = "skills__content skills__open"
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener("click", toggleSkills)
+})
+
+            /*________ QUALIFICATION TABS __________ */
+const tabs = document.querySelectorAll("[data-target]"),
+      tabContents = document.querySelectorAll("[data-content]")
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove("qualification__active")
+        })
+        target.classList.add("qualification__active")
+
+        tabs.forEach(tab => {
+            tab.classList.remove("qualification__active")
+        })
+        tab.classList.add("qualification__active")
+    })
+})
+
+            /*_________- SERVICES MODAL -_________ */
+const modalViews = document.querySelectorAll(".services__modal"),
+      modalBtns = document.querySelectorAll(".services__button"),
+      modalCloses = document.querySelectorAll(".services__modal-close")
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add("active-modal")
+}  
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener("click", () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener("click", () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove("active-modal")
+        })
+    })
+})
+
+            /*_______- PORTAFOLIO SWIPER- ________*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop:true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+});
